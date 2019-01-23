e24f2addc6e9a1a94a8fbdba13d8c81106d2b53a
erick.alvarenga0105 <erick.alvarenga0105@outlook.com>
2019-01-23 12:20:05 -0600

    just git things

---
+++ SpiralDocs/Controllers/DocumentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpiralDocs.Sevices; 
+
+namespace SpiralDocs.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        private readonly SpiralDocService _docService; 
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Index";
+
+            return View();
+        }
+    }
+}
